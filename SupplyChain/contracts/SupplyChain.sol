2f7d4545c0a16cdb361637a9a73d22e251203ca4
kenil0811 <kenilshah081198@gmail.com>
2019-11-07 19:38:07 +0530

    updates

--- SupplyChain/contracts/SupplyChain.sol
+++ SupplyChain/contracts/SupplyChain.sol
@@ -30,7 +30,7 @@ contract SupplyChain {
 
 
     function setRoles() private {
-        players[0x13A72135525FE16DcECb0C0BF59F1f730aF00e27] = Details(1, 10, 5, 0, 0xBF43C96c307B2c822fbc6Ae3fC6cf203D1c8062D, 0x13A72135525FE16DcECb0C0BF59F1f730aF00e27);
+        players[0x13A72135525FE16DcECb0C0BF59F1f730aF00e27] = Details(1, 10, 5, 100, 0xBF43C96c307B2c822fbc6Ae3fC6cf203D1c8062D, 0x13A72135525FE16DcECb0C0BF59F1f730aF00e27);
         players[0xBF43C96c307B2c822fbc6Ae3fC6cf203D1c8062D] = Details(2, 20, 15, 0, 0xFe536dc6ef2167B9184946FF21D084E16e5EAd67, 0x13A72135525FE16DcECb0C0BF59F1f730aF00e27);
         players[0xFe536dc6ef2167B9184946FF21D084E16e5EAd67] = Details(3, 30, 25, 0, 0x99C230F42Da0F5391cf4EA4dc4e2e6310eFDadB9, 0xBF43C96c307B2c822fbc6Ae3fC6cf203D1c8062D);
         players[0x99C230F42Da0F5391cf4EA4dc4e2e6310eFDadB9] = Details(4, 40, 35, 0, 0x99C230F42Da0F5391cf4EA4dc4e2e6310eFDadB9, 0xFe536dc6ef2167B9184946FF21D084E16e5EAd67);
@@ -40,6 +40,18 @@ contract SupplyChain {
        address upAddreess = players[msg.sender].upstream;
        players[upAddreess].orderReceived = _amt;
  //       emit orderPlaced(_orderAmount);
+    }
+
+    function fillStock(uint _amt) public {
+        players[msg.sender].inventory += _amt;
+        emit orderSent(_amt);
+    }
+
+    function clearStock(uint _amt) public {
+        require(_amt <= players[msg.sender].inventory && _amt<= players[msg.sender].orderReceived);
+        players[msg.sender].inventory -= _amt;
+        players[msg.sender].orderReceived -= _amt;
+        emit orderSent(_amt);
     }
 
     function orderDown(uint _amt) public {
