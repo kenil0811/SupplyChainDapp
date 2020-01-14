8a9c348f4504b3815014172a36279670fc34941b
GPrajwal12 <praj12121998@gmail.com>
2020-01-14 21:24:12 +0530

    errors

--- SupplyChain/src/js/new.js
+++ SupplyChain/src/js/new.js
@@ -74,14 +74,14 @@ App = {
         instance.weekNo().then(function(weeks){
           //console.log(weeks.c[0]);
           //console.log(numWeeks);
-        var a=0;
+        var a=1;
         for(i=0; i<weeks.c[0]; i++) {
             //console.log(i);
             //console.log(weeks.c[0]);
             instance.weekDetails(web3.eth.accounts,i).then(function(player){
                 //console.log(i);
               var table = document.getElementById("DetailsTable");
-              var row = table.insertRow(1);
+              var row = table.insertRow(a);
               var c = row.insertCell(0);
               c.innerHTML = a;
               a++;
