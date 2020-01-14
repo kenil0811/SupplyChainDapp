f03b52e3ac545d1a544c6a50c1b9723de38ac89a
GPrajwal12 <praj12121998@gmail.com>
2020-01-14 21:03:52 +0530

    error

--- SupplyChain/truffle-config.js
+++ SupplyChain/truffle-config.js
@@ -7,11 +7,11 @@ module.exports = {
   networks: {
   	development: {
   		host: "127.0.0.1",
-  		port: 7545,
+  		port: 8545,
   		network_id: "*"
   	},
   	develop: {
-  		port: 7545
+  		port: 8545
   	}
  	/*ropsten: {
 		provider: function() {
